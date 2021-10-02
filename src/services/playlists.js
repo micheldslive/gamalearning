149ea620d03013f4368da081803be025efa435b5
Michel Domingos <micheldslive@gmail.com>
2021-10-02 12:49:55 -0300

    first commit

--- src/services/playlists.js
+++ src/services/playlists.js
@@ -7,12 +7,12 @@ export const playlists = [
       {
         id: "1",
         name: "Oratória",
-        src: "http://gamaxp.s3-website.us-east-2.amazonaws.com/videos/01/01-Oratoria.mp4",
+        src: "https://github.com/micheldslive/gamalearning/raw/gamalearning/src/assets/videos/01/01-Oratoria.mp4",
       },
       {
         id: "2",
         name: "Coachability",
-        src: "http://gamaxp.s3-website.us-east-2.amazonaws.com/videos/01/02-Coachability.mp4",
+        src: "https://github.com/micheldslive/gamalearning/raw/gamalearning/src/assets/videos/01/02-Coachability.mp4",
       },
     ],
   },
@@ -25,27 +25,27 @@ export const playlists = [
       {
         id: "1",
         name: "O que é programação",
-        src: "http://gamaxp.s3-website.us-east-2.amazonaws.com/videos/02/01-O-que-e-programacao.mp4",
+        src: "https://github.com/micheldslive/gamalearning/raw/gamalearning/src/assets/videos/02/01-O-que-e-programacao.mp4",
       },
       {
         id: "2",
         name: "Dados do mercado",
-        src: "http://gamaxp.s3-website.us-east-2.amazonaws.com/videos/02/02-Dados-do-mercado.mp4",
+        src: "https://github.com/micheldslive/gamalearning/raw/gamalearning/src/assets/videos/02/02-Dados-do-mercado.mp4",
       },
       {
         id: "3",
         name: "Como é a carreira de desenvolvimento",
-        src: "http://gamaxp.s3-website.us-east-2.amazonaws.com/videos/02/03-Como-e-a-carreira-de-desenvolvimento.mp4",
+        src: "https://github.com/micheldslive/gamalearning/raw/gamalearning/src/assets/videos/02/03-Como-e-a-carreira-de-desenvolvimento.mp4",
       },
       {
         id: "4",
         name: "Linguagens, frameworks e bibliotecas",
-        src: "http://gamaxp.s3-website.us-east-2.amazonaws.com/videos/02/04-Linguagens-frameworks-e-bibliotecas.mp4",
+        src: "https://github.com/micheldslive/gamalearning/raw/gamalearning/src/assets/videos/02/04-Linguagens-frameworks-e-bibliotecas.mp4",
       },
       {
         id: "5",
         name: "Editores de texto",
-        src: "http://gamaxp.s3-website.us-east-2.amazonaws.com/videos/02/05-Editores-de-texto.mp4",
+        src: "https://github.com/micheldslive/gamalearning/raw/gamalearning/src/assets/videos/02/05-Editores-de-texto.mp4",
       },
     ],
   },
