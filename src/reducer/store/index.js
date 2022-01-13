9ec6d54ac15a5fae83937f79b5e4469e1fb8fcdf
Michel Domingos <micheldslive@gmail.com>
2022-01-13 12:20:28 -0300

    added redux-localstorage

--- src/reducer/store/index.js
+++ src/reducer/store/index.js
@@ -1,53 +1,6 @@
-import { playlists } from "services/playlists";
-import { defineState } from "redux-localstore";
 import { createStore, combineReducers } from "redux";
 import storeSynchronize from "redux-localstore";
-
-const defaultState = {
-  user: null,
-  aside: false,
-  checked: false,
-  activeLesson: 1,
-  activeModule: 1,
-  modules: playlists,
-  lessonArray: [],
-  moduleArray: [],
-};
-
-const initState = defineState(defaultState)('gama');
-
-const gama = (state = initState, action) => {
-  if (action.type === "SET_ASIDE") {
-    return {
-      ...state,
-      aside: action.aside,
-    };
-  }
-
-  if (action.type === "SET_CHECKED") {
-    return {
-      ...state,
-      checked: action.checked,
-    };
-  }
-
-  if (action.type === "SET_USER") {
-    return {
-      ...state,
-      user: action.user,
-    };
-  }
-
-  if (action.type === "ACTIVE_LESSON") {
-    return {
-      ...state,
-      activeLesson: action.lesson,
-      activeModule: action.module,
-    };
-  }
-
-  return state;
-};
+import { gama } from "reducer/state"
 
 const combineReducer = combineReducers({
   gama,
